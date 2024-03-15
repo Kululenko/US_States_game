e854624b6533049f4235599c8ec98beab0a3278b
kululenko <alex.pmail@yahoo.com>
2024-03-15 08:10:47 +0100

    fick dich

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+screen.title("U.S States Game")
+image = "C:\\Users\\pcmai\\Desktop\\python\\100days\\US_states_game\\blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+
+
+screen.exitonclick()
